356bee060af1570c4e62a2d067d2e65c4b7dffc7
Mason <chtuff@gmail.com>
2024-08-31 12:26:48 -0400

    solution

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+# PARAMS: 
+# RESULT: returns "Even" 
+#         for even numbers or "Odd" for odd numbers.
+#EXAMPLE:
+#PSUEDOCODE: use a conditional expression to 
+#            evaluate the variable.
+
+def even_or_odd(number):
+	return 'Odd' if number % 2 else 'Even'
