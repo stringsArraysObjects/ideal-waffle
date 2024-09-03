5f6b51e08ea3a4933ae3834b545dc22a01b717e1
Mason <chtuff@gmail.com>
2024-09-03 17:16:23 -0400

    solution

---
+++ number_to_string.py
@@ -0,0 +1,8 @@
+# PARAMS:
+# RESULTS: transform a number(integer) to a string
+# EXAMPLE:  123  --> "123"
+#           999  --> "999"
+# PSEUDOCODE:   use the str function to convert a num to a string.
+
+def number_to_string(num):
+    return str(num)
