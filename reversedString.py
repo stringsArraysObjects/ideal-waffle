abede647520b9f6dcc05f0609074444cfe817b1f
Mason <chtuff@gmail.com>
2024-09-04 11:25:03 -0400

    solution

---
+++ reversedString.py
@@ -0,0 +1,7 @@
+#PSEUDOCODE:  Use slicing to extract a portion of a string.
+#             In this case we will be omitting the start and stop
+#             and using the defealt behavior. Use -1 step to 
+#             traverse the string in reverse order.
+
+def solution(str):
+  return str[::-1]
