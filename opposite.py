7b9e889dafd66cff1a878b2cb0df152c009cd0ca
Mason <chtuff@gmail.com>
2024-09-05 15:02:21 -0400

    solution

---
+++ opposite.py
@@ -0,0 +1,8 @@
+# RESULTES: given a number (integer / decimal / both depending on the language), 
+#           find its opposite (additive inverse).
+# EXAMPLE: 1: -1
+#          14: -14
+
+
+def opposite(number):
+    return -number
