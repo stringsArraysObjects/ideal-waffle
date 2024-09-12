26d5335495695244451cd1c203adb876e620e283
Mason <chtuff@gmail.com>
2024-09-12 12:49:35 -0400

    solution

---
+++ remove_char.py
@@ -0,0 +1,6 @@
+#PARAMS: strings will have at least 2 chars.
+#RESULT:  Your goal is to create a function that removes 
+#         the first and last characters of a string.  
+
+def remove_char(s):
+    return s[1 : -1]
