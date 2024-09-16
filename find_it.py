65156d1b1662d5bc11b2f170be5cfee1ecf457ad
Mason <chtuff@gmail.com>
2024-09-16 10:37:22 -0400

    solution

--- find_it.py
+++ find_it.py
@@ -1,6 +1,7 @@
 #PARAMS: There will always be only one integer that appears an odd number of times.
 #RESULTS: Given an array of integers, find the one that appears an odd number of times.
 #EXAMPLE: [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+#PSEUDOCODE: use the count() method to count the number of occurrences in the sequence.
 
 def find_it(seq):
     return [x for x in seq if seq.count(x) % 2][0]
